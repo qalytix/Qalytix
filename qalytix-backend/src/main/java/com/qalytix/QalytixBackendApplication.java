f2a273325ce729b115be812bc145d68a25650376
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2026-05-23 18:52:38 +0530

    initial commit

---
+++ qalytix-backend/src/main/java/com/qalytix/QalytixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qalytix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QalytixBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QalytixBackendApplication.class, args);
+    }
+
+}
---
+++ qalytix-backend/src/test/java/com/qalytix/QalytixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qalytix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QalytixBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
